950ff3333b9d930ad2682cb0d037464997576d40
barbaros-durusoy <barbaros.durusoy@gmail.com>
2023-02-17 05:59:16 +0300

    Reworked MostFrequentSentenceAutoSemantic so that it gives precedence to multiword expressions over single words and looks for exact matches in literals instead of partial matches in determining the most frequent one

--- Test/Sentence/TestMostFrequent.cs
+++ Test/Sentence/TestMostFrequent.cs
@@ -37,7 +37,7 @@ namespace Test.Sentence
                 }
             }
             Assert.AreEqual(549, total);
-            Assert.AreEqual(277, correct);
+            Assert.AreEqual(269, correct);
         }
     }
 }
